482d932f8f0248701f9a14969c588b44c8f349e1
skjanani <41111202+skjanani@users.noreply.github.com>
2018-08-01 17:58:08 +0530

    Add files via upload

---
+++ Float.java
@@ -0,0 +1,15 @@
+package janani;
+import java.util.Scanner;
+public class Float {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float n;
+System.out.println("enter the number");
+Scanner ja=new Scanner(System.in);
+n=ja.nextFloat();
+int a=(int)n+1;
+System.out.println(a);
+	}
+
+}
